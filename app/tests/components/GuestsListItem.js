01475862fac275457eb9f13000bf1963d63cf179
Ryan Collins <rccproductions@mac.com>
2016-07-09 21:42:58 -0400

    Test: add component testing for guest list item

---
+++ app/tests/components/GuestsListItem.js
@@ -0,0 +1,11 @@
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import GuestsListItem from '../../src/components/Header';
+import GuestsListInput from '../../src/components/GuestsListInput';
+
+// function setup() {
+//   let props = {
+//
+//   };
+// }
